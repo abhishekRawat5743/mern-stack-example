d6a76982622dcf0ff92cde4c2e6535f7cc3ad258
Abhishek Rawat <abhishekrawa@IN-V57J4M62GT.local>
2025-06-09 16:45:04 +0530

    updated code for production

--- mern/server/server.js
+++ mern/server/server.js
@@ -1,9 +1,17 @@
-dotenv.config();
-
 import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
 import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: "./config.env" });
+
+// Serve frontend build
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 const PORT = process.env.PORT || 5050;
 const app = express();
